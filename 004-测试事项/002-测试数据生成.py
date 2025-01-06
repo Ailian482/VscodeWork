64559535447975433f42d5203e4f05d1321d3b82
Ailian482 <1479844033@qq.com>
2025-01-06 18:39:53 +0800

    add

--- 004-测试事项/002-测试数据生成.py
+++ 004-测试事项/002-测试数据生成.py
@@ -45,9 +45,9 @@ class GenerateChinese():
     
 
 # print(GenerateChinese().unicode_chinese(10))
-print(GenerateChinese().unicode_chinese(21))
+# print(GenerateChinese().unicode_chinese(21))
 print(GenerateChinese().ascii_string(10) + (GenerateChinese().unicode_chinese(190)))
-print(GenerateChinese().ascii_string(101))
+# print(GenerateChinese().ascii_string(101))
 
 """
 python 随机生成汉字的三种方法
